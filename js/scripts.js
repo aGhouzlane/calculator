fab21e06a43d47debee0fd24668299d288da5aee
Shane Graff <copellius@gmail.com>
2021-03-24 16:35:30 -0700

    Add buttons to index page.
    Co-authored-by: Ahmed Ghouzlane <ahmedghouzlane@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,49 @@
 function add(number1, number2) {
   return number1 + number2;
 }
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+};
+
+function divide(number1, number2) {
+  return number1 / number2;
+};
+
+
+$(document).ready(function () {
+
+  $("form#add").submit(function (event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    var operator = '';
+
+    $("#plus").click(function () {
+      const result = add(number1, number2);
+      $("#output").text(result);
+    });
+
+    $("#minus").click(function () {
+      const result = subtract(number1, number2);
+      $("#output").text(result);
+    });
+
+    $("#multiply").click(function () {
+      const result = multiply(number1, number2);
+      $("#output").text(result);
+    });
+
+    $("#divide").click(function () {
+      const result = divide(number1, number2);
+      $("#output").text(result);
+    });
+
+
+
+  });
+});
